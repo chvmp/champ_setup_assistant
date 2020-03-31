392bbb35e3debba094ca221c75ec62cb78bb4f30
Juan Jimeno <jimenojmm@gmail.com>
2020-04-01 01:44:15 +0800

    added new helpers to find foot links

--- scripts/urdf_parser.py
+++ scripts/urdf_parser.py
@@ -30,9 +30,14 @@ from urdf_parser_py.urdf import URDF
 from rosparam import upload_params
 from yaml import load
 import yaml
+from difflib import SequenceMatcher 
+import numpy as np
 
 class URDFParser():
     def __init__(self):
+        self.end_links = None
+        self.foot_links = None
+        self.foot_links_ns = None
         self.robot = None
         self.joints = None
         self.links = None
@@ -46,7 +51,9 @@ class URDFParser():
         self.joints = self.parse_joints(self.robot)
         self.link_names = self.parse_link_names(self.links)
         self.joint_names = self.parse_joint_names(self.joints)
-        self.base = self.link_names[0]
+        self.base = self.robot.get_root()
+        self.end_links = self.get_end_links()
+        self.foot_links, self.foot_links_ns = self.get_foot_links()
 
     def open_urdf(self, full_path):
         return URDF.from_xml_file(full_path)
@@ -146,5 +153,95 @@ class URDFParser():
         for joint in self.joints:
             if joint.name == joint_name:
                 return joint.origin.xyz, joint.origin.rpy
+    
+    def link_has_child(self, link_name):
+        return self.robot.child_map.has_key(link_name)
 
+    def link_attached_to_base(self, link_name):
+        attached_joint, parent_link = self.robot.parent_map[link_name]
+        #add more filter - if it's attached to base, it's not an end link
+        if parent_link != self.base:
+            return False
+        else:
+            return True
 
+    def joint_is_revolute(self, joint_name):
+        for joint in self.joints:
+            if joint.name == joint_name:
+                if joint.type == "revolute":
+                    return True
+
+        return False
+
+    def has_actuator_in_chain(self, link_name):
+        joints = self.get_joint_chain(link_name)
+        for joint in joints:
+            if self.joint_is_revolute(joint):
+                return True
+        
+        return False
+
+    def get_end_links(self):
+        end_links = []
+        for link in self.link_names:
+            #check if this link has a child
+            if not self.link_has_child(link) and not self.link_attached_to_base(link) and self.has_actuator_in_chain(link):
+                end_links.append(link)
+
+        return end_links
+    
+    def get_foot_links(self):
+        def get_common_string(str1,str2):
+            #returns common substring between two strings
+            #https://www.geeksforgeeks.org/sequencematcher-in-python-for-longest-common-substring/
+            # initialize SequenceMatcher object with  
+            # input string 
+            seqMatch = SequenceMatcher(None,str1,str2) 
+        
+            # find match of longest sub-string 
+            # output will be like Match(a=0, b=0, size=5) 
+            match = seqMatch.find_longest_match(0, len(str1), 0, len(str2)) 
+        
+            # print longest substring 
+            if (match.size!=0): 
+
+                return (str1[match.a: match.a + match.size])  
+            else: 
+                return None
+
+        #get links that has no child
+        end_links = self.get_end_links()
+        if len(end_links) == 4:
+            foot_name = get_common_string(end_links[0], end_links[3])
+            ns =[]
+            for link in end_links:
+                ns.append(link.replace(foot_name, ""))
+
+            return end_links, ns
+
+        else:
+            common_substrings = []
+            #iterate all end links to each other
+            for i in  end_links:
+                for ii in end_links:
+                    #get common substring between two link names
+                    common = get_common_string(i, ii)
+
+                    #make sure it's not some giberrish string
+                    if len(common) > 3:
+                        common_substrings.append(get_common_string(i, ii))
+            #count the most no of occurance in the collected substrings
+            strings, indices = np.unique(common_substrings, return_counts=True)
+            #name of the link without ns / identifier
+            foot_name = strings[0] 
+        
+            foot_links = []
+            ns = []
+            for link in end_links:
+                #check if the footname matches the end_links
+                if link.count(foot_name):
+                    foot_links.append(link)
+                    #remove footname on the link name to get namespace
+                    ns.append(link.replace(foot_name, ""))
+
+            return foot_links, ns
